c3d12670014a15707e3968cb0ccd91ff20d93df5
aleksandr <karpov.a1exandr@yandex.ru>
2022-07-17 14:59:49 +0300

    black format

--- chat_bot.py
+++ chat_bot.py
@@ -4,17 +4,17 @@ from random import randint
 from vk_api import bot_longpoll, VkApi
 import logging.config
 from logging_config import log_config
+
 try:
     from settings import TOKEN, GROUP_ID
 except ImportError:
-    exit('Do cp settings.py.default settings.py and set token')
+    exit("Do cp settings.py.default settings.py and set token")
 
 logging.config.dictConfig(log_config)
-log = logging.getLogger('chat_bot')
+log = logging.getLogger("chat_bot")
 
 
 class VkBot:
-
     def __init__(self, vk_token, group_id):
         self.api = VkApi(token=vk_token)
         self.group_id = group_id
@@ -26,19 +26,20 @@ class VkBot:
             try:
                 self.processing_event(event)
             except Exception as exc:
-                log.exception('событие обработано с ошибкой', exc)
+                log.exception("событие обработано с ошибкой", exc)
 
     def processing_event(self, event):
         if event.type is bot_longpoll.VkBotEventType.MESSAGE_NEW:
-            log.debug('отправляем сообщение назад')
-            self.api_method.messages.send(message='Все говорят ' + event.message.text + ', а ты купи слона',
-                                          user_id=event.message.from_id,
-                                          random_id=randint(0, 2 ** 20)
-                                          )
+            log.debug("отправляем сообщение назад")
+            self.api_method.messages.send(
+                message="Все говорят " + event.message.text + ", а ты купи слона",
+                user_id=event.message.from_id,
+                random_id=randint(0, 2**20),
+            )
         else:
-            log.info(f'Мы пока не умеем обрабатывать события типа {event.type}')
+            log.info(f"Мы пока не умеем обрабатывать события типа {event.type}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot = VkBot(vk_token=TOKEN, group_id=GROUP_ID)
     bot.act()
--- tests.py
+++ tests.py
@@ -7,19 +7,44 @@ from chat_bot import VkBot
 
 
 class ChatTests(TestCase):
-    RAW_EVENT = {'group_id': 102260837, 'type': 'message_new', 'event_id': '0a844246825213d6a2e28e7dbedfae1bfa6c0c03',
-                 'v': '5.131',
-                 'object': {
-                     'message':
-                         {'date': 1658042320, 'from_id': 83083975, 'id': 81, 'out': 0, 'attachments': [],
-                          'conversation_message_id': 81, 'fwd_messages': [], 'important': False, 'is_hidden': False,
-                          'peer_id': 83083975, 'random_id': 0, 'text': 'привет'},
-                     'client_info':
-                         {'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link', 'callback',
-                                             'intent_subscribe', 'intent_unsubscribe'],
-                          'keyboard': True, 'inline_keyboard': True, 'carousel': True, 'lang_id': 0}
-                            }
-                 }
+    RAW_EVENT = {
+        "group_id": 102260837,
+        "type": "message_new",
+        "event_id": "0a844246825213d6a2e28e7dbedfae1bfa6c0c03",
+        "v": "5.131",
+        "object": {
+            "message": {
+                "date": 1658042320,
+                "from_id": 83083975,
+                "id": 81,
+                "out": 0,
+                "attachments": [],
+                "conversation_message_id": 81,
+                "fwd_messages": [],
+                "important": False,
+                "is_hidden": False,
+                "peer_id": 83083975,
+                "random_id": 0,
+                "text": "привет",
+            },
+            "client_info": {
+                "button_actions": [
+                    "text",
+                    "vkpay",
+                    "open_app",
+                    "location",
+                    "open_link",
+                    "callback",
+                    "intent_subscribe",
+                    "intent_unsubscribe",
+                ],
+                "keyboard": True,
+                "inline_keyboard": True,
+                "carousel": True,
+                "lang_id": 0,
+            },
+        },
+    }
 
     def test_act(self):
         count = 5
@@ -28,9 +53,12 @@ class ChatTests(TestCase):
         long_poller_listen_mock = Mock()
         long_poller_listen_mock.listen = long_poller_mock
 
-        with patch('chat_bot.VkApi'):
-            with patch('chat_bot.bot_longpoll.VkBotLongPoll', return_value=long_poller_listen_mock):
-                bot = VkBot('', '')
+        with patch("chat_bot.VkApi"):
+            with patch(
+                "chat_bot.bot_longpoll.VkBotLongPoll",
+                return_value=long_poller_listen_mock,
+            ):
+                bot = VkBot("", "")
                 bot.processing_event = Mock()
                 bot.act()
 
@@ -42,17 +70,18 @@ class ChatTests(TestCase):
         event = VkBotMessageEvent(raw=self.RAW_EVENT)
         send_mock = Mock()
 
-        with patch('chat_bot.VkApi'):
-            with patch('chat_bot.bot_longpoll.VkBotLongPoll'):
-                bot = VkBot('', '')
+        with patch("chat_bot.VkApi"):
+            with patch("chat_bot.bot_longpoll.VkBotLongPoll"):
+                bot = VkBot("", "")
                 bot.api = Mock()
                 bot.api_method.messages.send = send_mock
 
                 bot.processing_event(event)
 
         send_mock.assert_called_once_with(
-                                     message='Все говорят ' + self.RAW_EVENT['object']['message']['text'] +
-                                             ', а ты купи слона',
-                                     user_id=self.RAW_EVENT['object']['message']['from_id'],
-                                     random_id=ANY
-                                            )
+            message="Все говорят "
+            + self.RAW_EVENT["object"]["message"]["text"]
+            + ", а ты купи слона",
+            user_id=self.RAW_EVENT["object"]["message"]["from_id"],
+            random_id=ANY,
+        )
